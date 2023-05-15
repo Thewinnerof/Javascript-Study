fc7cc4594c3bd21bed2b6f42842dacf55b04079c
Thewinnerof <1020343877@qq.com>
2023-05-15 15:26:35 +0800

    no message

---
+++ Day01/javascript01.js
@@ -0,0 +1,18 @@
+// 输出内容
+// document.write("向body标签内写入内容")
+
+// 写入到控制台
+console.log(123);
+
+// 定义一个函数
+function myFunction(a, b) {
+	// return a * b;
+	console.log(a * b);
+}
+
+a = 5;
+b = 6;
+myFunction(a, b);
+
+
+// html 文本输出内容 - innerHtml
