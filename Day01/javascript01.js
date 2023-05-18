ff5f57e091791a2117c99e83c8c68b99cf8e8f79
Thewinnerof <1020343877@qq.com>
2023-05-18 09:21:00 +0800

    no message

--- Day01/javascript01.js
+++ Day01/javascript01.js
@@ -14,9 +14,34 @@ a = 5;
 b = 6;
 myFunction(a, b);
 
-
 // html 文本输出内容 - innerHtml
-
 
 // 作用域
 
+// 显式函数绑定
+var person1 = {
+	fullName: function () {
+		console.log(this.firstName + " " + this.lastName);
+	},
+};
+
+var person2 = {
+	firstName: "Mike",
+	lastName: "Angle",
+};
+
+person1.fullName.call(person2); // Mike Angle
+
+// 局部变量
+
+// 这里不可以使用carName变量
+function myFunction1() {
+	var carName = "Volvo";
+	// 这里可以使用carName变量
+}
+try{
+	console.log(carName);
+}catch{
+	console.log("此处调用变量carName错误");
+}
+// 这里不可以使用carName变量
